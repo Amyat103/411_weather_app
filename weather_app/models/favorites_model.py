a0dfff68e4268cca2fa0146212fcc2f9e10650dc
strumm36 <ostrum@bu.edu>
2024-12-04 22:13:50 -0500

    progress on favorites model

--- weather_app/models/favorites_model.py
+++ weather_app/models/favorites_model.py
@@ -3,9 +3,8 @@ import os
 import time
 from typing import Any, List
 
-from weather_app.models.kitchen_model import Meals
+from weather_app.models.location_model import Locations
 from weather_app.utils.logger import configure_logger
-from weather_app.utils.random_utils import get_random
 
 
 logger = logging.getLogger(__name__)
@@ -17,142 +16,230 @@ TTL = os.getenv("TTL", 60)  # Default TTL is 60 seconds
 
 class FavoritesModel:
     """
-    A class to manage a playlist of songs.
+    A class to manage a favorites list of locations.
 
     Attributes:
-        current_track_number (int): The current track number being played.
-        playlist (List[Song]): The list of songs in the playlist.
+        favorite_location (int): The current favorite location.
+        favorites (List[Locations]): The list of favorited locations.
 
     """
 
     def __init__(self):
         """
-        Initializes the PlaylistModel with an empty playlist and the current track set to 1.
+        Initializes the FavoritesModel with an empty favorites and the current favorite set to 1.
         """
-        self.current_track_number = 1
-        self.playlist: List[Song] = []
+        self.favorite_location = 1
+        self.favorites: List[Locations] = []
 
     ##################################################
     # Song Management Functions
     ##################################################
 
-    def add_song_to_playlist(self, song: Song) -> None:
+    def add_location_to_favorites(self, location: Locations) -> None:
         """
-        Adds a song to the playlist.
+        Adds a location to the favorites.
 
         Args:
-            song (Song): the song to add to the playlist.
+            location (Locations): the location to add to the favorites.
 
         Raises:
-            TypeError: If the song is not a valid Song instance.
-            ValueError: If a song with the same 'id' already exists.
+            TypeError: If the location is not a valid Locations instance.
+            ValueError: If a location with the same 'id' already exists.
         """
-        logger.info("Adding new song to playlist")
-        if not isinstance(song, Song):
-            logger.error("Song is not a valid song")
-            raise TypeError("Song is not a valid song")
+        logger.info("Adding new location to favorites")
+        if not isinstance(location, Locations):
+            logger.error("Location is not a valid location")
+            raise TypeError("Location is not a valid location")
 
-        song_id = self.validate_song_id(song.id, check_in_playlist=False)
-        if song_id in [song_in_playlist.id for song_in_playlist in self.playlist]:
-            logger.error("Song with ID %d already exists in the playlist", song.id)
-            raise ValueError(f"Song with ID {song.id} already exists in the playlist")
+        location_id = self.validate_location_id(location.id, check_in_favorites=False)
+        if location_id in [i.id for i in self.favorites]:
+            logger.error("Location with ID %d already exists in favorites", location.id)
+            raise ValueError(f"Location with ID {location.id} already exists in favorites")
 
-        self.playlist.append(song)
+        self.favorites.append(location)
 
-    def remove_song_by_song_id(self, song_id: int) -> None:
+    def remove_location_by_location_id(self, location_id: int) -> None:
         """
-        Removes a song from the playlist by its song ID.
+        Removes a location from the favorites by its location ID.
 
         Args:
-            song_id (int): The ID of the song to remove from the playlist.
+            location_id (int): The ID of the location to remove from the favorites.
 
         Raises:
-            ValueError: If the playlist is empty or the song ID is invalid.
+            ValueError: If the favorites is empty or the location ID is invalid.
         """
-        logger.info("Removing song with id %d from playlist", song_id)
+        logger.info("Removing location with id %d from favorites", location_id)
         self.check_if_empty()
-        song_id = self.validate_song_id(song_id)
-        self.playlist = [song_in_playlist for song_in_playlist in self.playlist if song_in_playlist.id != song_id]
-        logger.info("Song with id %d has been removed", song_id)
+        location_id = self.validate_location_id(location_id)
+        self.favorites = [i for i in self.favorites if i.id != location_id]
+        logger.info("Location with id %d has been removed", location_id)
 
-    def remove_song_by_track_number(self, track_number: int) -> None:
+    def remove_location_by_index(self, index: int) -> None:
         """
-        Removes a song from the playlist by its track number (1-indexed).
+        Removes a location from the favorites by an index (1-indexed).
 
         Args:
-            track_number (int): The track number of the song to remove.
+            index (int): The index of the location to remove.
 
         Raises:
-            ValueError: If the playlist is empty or the track number is invalid.
+            ValueError: If the favorites is empty or the index is invalid.
         """
-        logger.info("Removing song at track number %d from playlist", track_number)
+        logger.info("Removing song at track number %d from playlist", index)
         self.check_if_empty()
-        track_number = self.validate_track_number(track_number)
-        playlist_index = track_number - 1
-        logger.info("Removing song: %s", self.playlist[playlist_index].title)
-        del self.playlist[playlist_index]
+        index = self.validate_index(index)
+        favorites_index = index - 1
+        logger.info("Removing location: %s", self.favorites[favorites_index].location)
+        del self.favorites[favorites_index]
 
-    def clear_playlist(self) -> None:
+    def clear_favorites(self) -> None:
         """
-        Clears all songs from the playlist. If the playlist is already empty, logs a warning.
+        Clears all locations from the favorites. If the favorites is already empty, logs a warning.
         """
-        logger.info("Clearing playlist")
-        if self.get_playlist_length() == 0:
-            logger.warning("Clearing an empty playlist")
-        self.playlist.clear()
+        logger.info("Clearing favorites")
+        if self.get_favorites_length() == 0:
+            logger.warning("Clearing an empty favorites")
+        self.favorites.clear()
 
     ##################################################
-    # Playlist Retrieval Functions
+    # Favorites Retrieval Functions
     ##################################################
 
-    def get_all_songs(self) -> List[Song]:
+    def get_all_favorites(self) -> List[Locations]:
         """
-        Returns a list of all songs in the playlist.
+        Returns a list of all locations in the favorites.
         """
         self.check_if_empty()
-        logger.info("Getting all songs in the playlist")
-        return self.playlist
+        logger.info("Getting all locations in the favorites")
+        return self.favorites
 
-    def get_song_by_song_id(self, song_id: int) -> Song:
+    def get_location_by_location_id(self, location_id: int) -> Locations:
         """
-        Retrieves a song from the playlist by its song ID.
+        Retrieves a song from the favorites by its location ID.
 
         Args:
-            song_id (int): The ID of the song to retrieve.
+            location_id (int): The ID of the location to retrieve.
 
         Raises:
-            ValueError: If the playlist is empty or the song is not found.
+            ValueError: If the favorites is empty or the location is not found.
         """
         self.check_if_empty()
-        song_id = self.validate_song_id(song_id)
-        logger.info("Getting song with id %d from playlist", song_id)
-        return next((song for song in self.playlist if song.id == song_id), None)
+        location_id = self.validate_location_id(location_id)
+        logger.info("Getting location with id %d from favorites", location_id)
+        return next((location for location in self.favorites if location.id == location_id), None)
 
-    def get_song_by_track_number(self, track_number: int) -> Song:
+    def get_location_by_index(self, index: int) -> Locations:
         """
-        Retrieves a song from the playlist by its track number (1-indexed).
+        Retrieves a location from the favorites by an index (1-indexed).
 
         Args:
-            track_number (int): The track number of the song to retrieve.
+            index(int): The index of the location to retrieve.
 
         Raises:
-            ValueError: If the playlist is empty or the track number is invalid.
+            ValueError: If the favorites is empty or the index is invalid.
         """
         self.check_if_empty()
-        track_number = self.validate_track_number(track_number)
-        playlist_index = track_number - 1
-        logger.info("Getting song at track number %d from playlist", track_number)
-        return self.playlist[playlist_index]
+        index = self.validate_index(index)
+        favorites_index = index - 1
+        logger.info("Getting location at index %d from favorites", index)
+        return self.favorites[favorites_index]
 
-    def get_current_song(self) -> Song:
+    def get_current_favorite(self) -> Locations:
         """
-        Returns the current song being played.
+        Returns the current favorite location.
         """
         self.check_if_empty()
-        return self.get_song_by_track_number(self.current_track_number)
+        return self.get_location_by_index(self.favorite_location)
 
-    def get_playlist_length(self) -> int:
+    def get_favorites_length(self) -> int:
         """
-        Returns the number of songs in the playlist.
+        Returns the number of locations in the favorites.
         """
-        return len(self.playlist)
+        return len(self.favorites)
+
+    def set_favorite_by_index(self, index: int):
+        self.favorite_location = index
+
+    ##################################################
+    # Favorites Weather Functions
+    ##################################################
+
+    def get_weather_for_location(self, location: Locations) -> str:
+        return ("Location: " + location.location + "\n"
+                + "Latitude: " + location.latitude + "\n"
+                + "Longitude: " + location.longitude + "\n"
+                + "Current Temperature: " + location.current_temperature + "\n"
+                + "Current Wind Speed: " + location.current_wind_speed + "\n"
+                + "Current Rain: " + location.current_rain + "\n\n")
+    
+    def get_weather_for_all_favorites(self) -> List[str]:
+        weathers = []
+        for i in range(len(self.favorites)):
+            weathers = weathers + self.get_weather_for_location(self.favorites[i])
+        return weathers
+    
+    def get_weather_for_favorite(self):
+        return self.get_weather_for_location(self.get_current_favorite())
+
+    ##################################################
+    # Utility Functions
+    ##################################################
+
+    def validate_location_id(self, location_id: int, check_in_favorites: bool = True) -> int:
+        """
+        Validates the given location ID, ensuring it is a non-negative integer.
+
+        Args:
+            location_id (int): The song ID to validate.
+            check_in_favorites (bool, optional): If True, checks if the location ID exists in the favorites.
+                                                If False, skips the check. Defaults to True.
+
+        Raises:
+            ValueError: If the location ID is not a valid non-negative integer.
+        """
+        try:
+            location_id = int(location_id)
+            if location_id < 0:
+                logger.error("Invalid location id %d", location_id)
+                raise ValueError(f"Invalid location id: {location_id}")
+        except ValueError:
+            logger.error("Invalid location id %s", location_id)
+            raise ValueError(f"Invalid location id: {location_id}")
+
+        if check_in_favorites:
+            if location_id not in [i.id for i in self.favorites]:
+                logger.error("Location with id %d not found in favorites", location_id)
+                raise ValueError(f"Location with id {location_id} not found in favorites")
+
+        return location_id
+
+    def validate_index(self, index: int) -> int:
+        """
+        Validates the given index, ensuring it is a non-negative integer within the favorites's range.
+
+        Args:
+            index (int): The index to validate.
+
+        Raises:
+            ValueError: If the index is not a valid non-negative integer or is out of range.
+        """
+        try:
+            index = int(index)
+            if index < 1 or index > self.get_favorites_length():
+                logger.error("Invalid index %d", index)
+                raise ValueError(f"Invalid index: {index}")
+        except ValueError:
+            logger.error("Invalid index %s", index)
+            raise ValueError(f"Invalid index: {index}")
+
+        return index
+
+    def check_if_empty(self) -> None:
+        """
+        Checks if the favorites is empty, logs an error, and raises a ValueError if it is.
+
+        Raises:
+            ValueError: If the favorites is empty.
+        """
+        if not self.favorites:
+            logger.error("Favorites is empty")
+            raise ValueError("Favorites is empty")
